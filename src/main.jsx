88640acd16a3c258c20b376e2edfd5e609a6ecd8
Jaehyeong Park <qkrwogud0623@ajou.ac.kr>
2025-10-08 21:58:42 +0900

    쪠에ㅔㅇ에에에ㅔㅇ발

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-// src/main.jsx
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -12,28 +10,18 @@ import SignupPage from './pages/SignupPage.jsx';
 import MainMapPage from './pages/MainMapPage.jsx';
 
 const router = createBrowserRouter([
-  // 그룹 1: 헤더가 없는 페이지들
   { path: "/welcome", element: <LandingPage /> },
   { path: "/login", element: <LoginPage /> },
   { path: "/signup", element: <SignupPage /> },
-  
-  // 그룹 2: 헤더가 있는 메인 앱 페이지들
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        index: true, 
-        element: <MainMapPage />,
-      },
-      {
-        path: "map",
-        element: <MainMapPage />,
-      },
+      { index: true, element: <MainMapPage /> },
+      { path: "map", element: <MainMapPage /> },
     ]
   }
 ], {
-  // ★★★ 바로 이 부분이 추가되어야 합니다! ★★★
   basename: "/Sori/"
 });
 
